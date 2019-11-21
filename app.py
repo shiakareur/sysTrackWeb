d18a2876ff7b62f18e6500560545c295d662a73d
Alejandro Macedo <a.macedo@pucp.pe>
2019-11-21 02:40:43 -0500

    Se agrego la base para los templates y el banner

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/indicadores")
+def indicadores():
+    return render_template("indicadores.html")
+
 if __name__=="__main__":
     app.run()
 
