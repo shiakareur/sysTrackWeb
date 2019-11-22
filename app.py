bbdb52ac3e7b5a4f3b478805dd537dca61b9469d
Silvia Antayhua <silvia.antayhua@pucp.edu.pe>
2019-11-22 11:13:21 -0500

    Avance de la zorrita

--- app.py
+++ app.py
@@ -6,7 +6,31 @@ app=Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route("/indicadores")
+@app.route("/Escuelaclj")
+def escuelaclj():
+    return render_template("escuelaclj.html")
+
+@app.route("/Hitos")
+def hitos():
+    return render_template("hitos.html")
+
+@app.route("/Malla")
+def malla():
+    return render_template("malla.html")
+
+@app.route("/Testimonios")
+def testimonios():
+    return render_template("testimonios.html")
+
+@app.route("/Instructores")
+def instructores():
+    return render_template("instructores.html")
+
+@app.route("/Postclj")
+def postclj():
+    return render_template("postclj.html")
+
+@app.route("/Indicadores")
 def indicadores():
     return render_template("indicadores.html")
 
